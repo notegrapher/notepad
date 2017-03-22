05e0edbcd149ab5b6b2fc27b46c78fae5309cc8e
notegrapher <notegrapher@gmail.com>
2017-03-22 23:55:29 +0900

    practice

--- Python/ageCalculator.py
+++ Python/ageCalculator.py
@@ -4,7 +4,7 @@ import datetime
 now = datetime.datetime.now()
 nowTuple = now.timetuple()
 # test
-# print(nowTuple.tm_year)
+print("오늘의 년도는", nowTuple.tm_year, " 입니다.")
 
 birthYear = input("태어난 년도를 입력하세요 : ")
 print("당신의 만 나이는 {}입니다.".format(int(nowTuple.tm_year)-int(birthYear)))
