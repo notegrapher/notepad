904c891f56781ec8c792719cd1fed6ecc014b327
KunYoung Heo <notegrapher@gmail.com>
2017-04-02 09:59:56 +0900

    practice

--- PHP/star.php
+++ PHP/star.php
@@ -6,10 +6,10 @@
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-  <form action="/star_print.php" method="get">
+  <form action="star_print.php" method="get">
   Print star! <br>
   Input the line number : <input class="form-control" type="number" placeholder="input the value">
-  <submit
+  <input type="submit" value="Run!"> 
   </form>
 </body>
 </html>
