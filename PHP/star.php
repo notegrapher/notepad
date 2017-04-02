7a37c3efd30ec56ba235d755497362e60a7cab3c
notegrapher <notegrapher@gmail.com>
2017-04-02 09:56:29 +0900

    practice

---
+++ PHP/star.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>PHP practice</title>
+  <meta lang="en">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+  <form action="/star_print.php" method="get">
+  Print star! <br>
+  Input the line number : <input class="form-control" type="number" placeholder="input the value">
+  <submit
+  </form>
+</body>
+</html>
