2e89cdfe20288031f9f60336251c1cb76fdbcefe
gyheo <heo.geoyeo@gmail.com>
2019-09-03 23:57:19 +0900

    Learn let, var and const

--- JavaScript/vanila-js.js
+++ JavaScript/vanila-js.js
@@ -1,5 +1,11 @@
 let x = 20;
 let y = 10;
 let sum = x + y;
+const myName = "HEO GEONYEONG";
 
-alert("x + y = " + sum);
+// alert("x + y = " + sum);
+alert("My name is " + myName);
+
+// Error
+// myName = 'a';
+// alert("My name is " + myName);
