60e08c07c7401b80ebb502735e4c30c8059a0951
gyheo <heo.geoyeo@gmail.com>
2019-09-20 23:58:24 +0900

    Edit HTML, CSS and JavaScript

--- JavaScript/vanila-js.js
+++ JavaScript/vanila-js.js
@@ -11,8 +11,12 @@ name.textContent = "Heo GeonYeong";
 let intro = document.querySelector('#intro');
 intro.textContent = "I just wanna learn🏃‍♂️ and learn📖";
 
+let language = document.querySelector('#language');
+language.textContent = "JavaScript, Java, Python, C++";
+
 let travel = document.querySelector('#travel');
 travel.textContent = "🇯🇵🇺🇸🇰🇿🇨🇦🇨🇳"
+
 
 // Error when edit const
 // const name = "HEO GEONYEONG";
